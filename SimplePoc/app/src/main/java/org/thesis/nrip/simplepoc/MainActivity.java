fc713ed77a4cfe530ea6066a410de727f3a6a802
Nicolas Martin Ravasi <nicolas.ravasi@mercadolibre.com>
2016-04-17 20:04:40 -0300

    add simple poc project

---
+++ SimplePoc/app/src/main/java/org/thesis/nrip/simplepoc/MainActivity.java
@@ -0,0 +1,51 @@
+package org.thesis.nrip.simplepoc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button first;
+    Button second;
+    EditText input;
+    TextView output;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        first = (Button) findViewById(R.id.first_button);
+//        second = (Button) findViewById(R.id.second_button);
+        input = (EditText) findViewById(R.id.input);
+        output = (TextView) findViewById(R.id.output);
+
+        first.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = input.getText().toString();
+
+                if(text.equals("1")){
+                    output.setText("Ganaste!");
+                }else{
+                    output.setText("Perdiste!");
+                }
+
+                output.setVisibility(View.VISIBLE);
+            }
+        });
+
+//        second.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                throw new RuntimeException("Hiciste click donde no tenias que");
+//            }
+//        });
+    }
+
+
+}
