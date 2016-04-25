00cdbc1482854b780bf1d77870aa085221f4dd69
Nicolas Martin Ravasi <nicolas.ravasi@mercadolibre.com>
2016-04-25 00:17:59 -0300

    missing files

---
+++ annotatorLogger/mainApp/src/main/java/com/tesis/annotatorLogger/mainApp/App.java
@@ -0,0 +1,16 @@
+package com.tesis.annotatorLogger.mainApp;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        try {
+            if ("hello".equals("hello")) {
+                int a = 1 / 0;
+            } else {
+            }
+        } catch (Exception e) {
+
+        }
+    }
+}
---
+++ annotatorLogger/processor/src/main/java/com/tesis/annotatorLogger/processors/CatchProcessor.java
@@ -0,0 +1,23 @@
+package com.tesis.annotatorLogger.processors;
+
+import org.apache.log4j.Level;
+import spoon.processing.AbstractProcessor;
+import spoon.processing.Severity;
+import spoon.reflect.code.CtCatch;
+
+/**
+ * Reports warnings when empty catch blocks are found.
+ */
+public class CatchProcessor extends AbstractProcessor<CtCatch> {
+
+    public void process(CtCatch element) {
+        if (element.getBody().getStatements().size() == 0) {
+            getFactory().getEnvironment().report(this, Severity.WARNING, element, "Something");
+            element.getBody().setDocComment("aasdasdasdasd");
+            System.out.println("Hello World!1");
+        } else {
+            element.getBody().setDocComment("aasdasdasdasd");
+            System.out.println("Hello World2!");
+        }
+    }
+}
